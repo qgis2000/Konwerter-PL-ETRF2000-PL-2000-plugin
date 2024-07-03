944cba2c343ad411420ff275f5f186bbb81efa2b
qgis2000 <142023542+qgis2000@users.noreply.github.com>
2024-07-03 17:53:33 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# __init__.py  -  Coordinates converter from PL-ETRF2000 to PL-2000 - QGIS plugin in python
+#     begin             : 2024-05-10
+#     version           : 1.1.2
+#.....version date......: 2024-07-03
+#     author            : Szymon Kędziora
+
+def classFactory(iface):
+    from .konwerterPLETRF2000PL2000Plugin import KonwerterPLETRF2000PL2000Plugin
+    return KonwerterPLETRF2000PL2000Plugin(iface)
+    
